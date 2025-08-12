22c91c6d41061350cfd2f40a25ac85f25e5b0f36
Adil Yunus <adilyunus@dsw.nl>
2025-08-12 18:03:51 +0200

    chapter 5

---
+++ ConsoleTestApp/Chapter4.cs
@@ -0,0 +1,107 @@
+﻿namespace ConsoleTestApp;
+
+public class Chapter4
+{
+    public static void Exercises()
+    {
+
+        string comparison = string.Empty;
+
+        Console.WriteLine("Vul een getal in:");
+
+        int eersteNr = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Vul nog een getal in:");
+
+        int tweedeNr = Convert.ToInt32(Console.ReadLine());
+
+        //Exercises 4-1-------------------
+
+        if (eersteNr < tweedeNr)
+        {
+            comparison = "kleiner dan";
+        }
+        else
+        {
+            comparison = (eersteNr == tweedeNr) ? "gelijk aan" : "groter dan";
+
+        }
+        Console.WriteLine($"Het eerste nummer is {comparison} het tweede nummer.");
+
+        Console.ReadKey();
+
+        //Exercises 4-2-------------------
+
+        if (eersteNr < tweedeNr)
+        {
+            comparison = "kleiner dan";
+        }
+
+        else if (eersteNr == tweedeNr)
+        {
+            comparison = "gelijk aan";
+        }
+        else
+        {
+            comparison = "groter dan";
+        }
+
+        //Exercise 4-3-----------------------
+
+        /* improves code readability, maintainability, and can potentially lead to better performance.*/
+
+        Console.WriteLine("Het eerste nummer is {0} het tweede nummer.", comparison);
+        Console.WriteLine($"Het eerste nummer is {comparison} het tweede nummer.");
+
+        Console.ReadKey();
+
+        /* exercise 4-4 --------------*/
+        Console.WriteLine("Written out numbers (text):");
+        string? getalTekst = Console.ReadLine()?.ToLower();
+
+        string nummer;
+
+        switch (getalTekst)
+        {
+            case "nul":
+                nummer = "0";
+                break;
+            case "een":
+                nummer = "1";
+                break;
+            case "twee":
+                nummer = "2";
+                break;
+            case "drie":
+                nummer = "3";
+                break;
+            case "vier":
+                nummer = "4";
+                break;
+            case "vijf":
+                nummer = "5";
+                break;
+            default:
+                nummer = "onbekend";
+                break;
+        }
+
+        Console.WriteLine($"Het nummer is {nummer}.");
+        Console.ReadKey();
+        /* Exercise 4-5 -----------*/
+        //Het nummer is onbekend
+
+        /* Exercise 4-6--------*/
+
+        for (int i = 0; i <= 10; i++)
+
+        {
+            i++;//i wordt nog een keer verhoogd
+                // 1,3,5,7,9,11 worden geprint
+            Console.WriteLine($"Het nummer is {i}.");
+        }
+
+        Console.ReadKey();
+
+    }
+}
---
+++ ConsoleTestApp/Chapter5.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ConsoleTestApp;
+
+namespace ConsoleTestApp;
+
+public class Chapter5
+{
+    private enum WindrichtingType : byte
+
+    {
+        Noord = 1,
+        Oost = 2,
+        Zuid = 3,
+        West = 4
+    }
+    
+    public static void Exercise1()
+    {
+        /* Exercise 5-1 */
+
+        
+        bool a = true;
+        string ab = Convert.ToString(a);
+        Console.WriteLine($"Boolean {a} to string {ab}");
+
+        ab = "false";
+        bool b = Convert.ToBoolean(ab);
+        Console.WriteLine($"String {ab} to boolean {b}");
+
+    }
+
+    public static void Exercise2()
+    {
+        int intGetal = 12345;
+        long longGetal = intGetal;
+        Console.WriteLine($"het longGetal is {longGetal}");
+
+        longGetal = 1234567890;
+        intGetal = Convert.ToInt32(longGetal);
+        Console.WriteLine($"intGetal is {intGetal}");
+
+    }
+
+    public static void Exercise3()
+    {
+        WindrichtingType windrichting = WindrichtingType.Noord;
+        
+        Console.WriteLine($"De windrichting {(byte)windrichting}");
+        Console.WriteLine($"De windrichting value naam is '{Convert.ToString(windrichting)}'");
+        windrichting = (WindrichtingType)Enum.Parse(typeof(WindrichtingType), "West");
+        Console.WriteLine(windrichting);
+
+        // convert a byte to WindrichtingType.
+        byte windByte = 2;
+        WindrichtingType windFromByte = (WindrichtingType)windByte;
+        Console.WriteLine($"Byte waarde {windByte} komt overeen met Windrichting '{windFromByte}'");
+
+        // convert an integer to WindrichtingType.
+        int windInt = 4;
+        WindrichtingType windFromInt = (WindrichtingType)windInt;
+        Console.WriteLine($"Int waarde {windInt} komt overeen met Windrichting '{windFromInt}'");
+
+    }
+    public static void Exercise4en5()
+        //exercise 5-4 and 5-5
+    {
+        
+        String[] namen = new string[5] {"Trump","Rutte","Elizabeth","Erdogan","Polo"};
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"De naam is '{namen[i]}'");
+        }
+    }
+
+    private const int MAX = 3;
+    public static void Exercise6(){
+        //exercise 5-6
+        {
+            Console.WriteLine($"De huidige datum is {DateTime.Now:yy-MM-dd}");
+            Console.WriteLine($"De huidige datum is {DateTime.Now:dd MMMM,yyyy}");
+            Console.WriteLine($"De huidige datum is {DateTime.Now:yyyy}");
+
+        }
+
+    }
+
+    public static void Exercise7()
+    {
+        Console.WriteLine("Voer een willekeurig woord in:");
+
+        string invoer = Console.ReadLine();
+
+       invoer = char.ToUpper(invoer[0]) + invoer.Substring(1).ToLower();
+
+        Console.WriteLine($"De gecorrigeerde invoer is {invoer}" );
+
+        
+    }
+
+    public static void Exercise8()
+    {
+        Console.WriteLine("Voer een willekeurig woord in:");
+
+        string invoer = Console.ReadLine();
+        
+        Console.WriteLine(invoer.Replace('e', 'E'));
+
+    }
+    public static void Exercise9()
+    {
+        string regel = "May the Force be with you. I'll be back. Life is like a box of chocolates.";
+        string[] zinnen = regel.Split('.');
+        
+        foreach (var zin in zinnen)
+
+        {
+            string trimmedZin = zin.Trim();
+
+            if (trimmedZin.Length == 0)
+                continue;
+            Console.WriteLine($"De zin is: {trimmedZin}");
+        }
+        
+    }
+
+}
+    
