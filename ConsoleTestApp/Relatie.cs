59780711dfb4b5725ea2fda10355dbcdbe33969d
Adil Yunus <adil.yunus@gmail.com>
2025-08-27 10:47:24 +0200

    exercise 1

---
+++ ConsoleTestApp/Relatie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestApp
+{
+    public class Relatie : RelatieBase
+    {
+        public Relatie(int burgerservicenummer, DateTime geboortedatum, GeslachtType geslacht, string voornaam, string geslachtsnaam)
+            : base(burgerservicenummer, geboortedatum, geslacht, voornaam, geslachtsnaam)
+        {
+        }
+
+        public Relatie(int burgerservicenummer, DateTime geboortedatum, GeslachtType geslacht, string voornaam, string geslachtsnaam, int nummer, int uzovicode)
+            : base(burgerservicenummer, geboortedatum, geslacht, voornaam, geslachtsnaam, nummer, uzovicode)
+        {
+        }
+    }
+}
